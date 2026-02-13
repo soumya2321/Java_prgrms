7cd851d62824a11d629d8bc046baa83920c493cb
Soumya <sahukarsoumya6@gmail.com>
2026-02-13 20:02:18 +0530

    Add files via upload
    printing Student Details with class and object

---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student{
+String name;
+int age;
+
+public static void main(String[ ] args){
+Student S1=new Student();
+S1.name="Soumya";
+S1.age = 21;
+System.out.println(S1.name);
+System.out.println(S1.age);
+}
+}
